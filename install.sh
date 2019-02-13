1117cb8729360666c7a30dcd1de14dc36687d473
jDan734 <frends734@bk.ru>
2019-02-13 21:13:39 +0300

    first commit

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo ""
+echo ""
+echo "      +---------------------------+"
+echo "      |                           |"
+echo "      |  revival-flat-apps-icons  |"
+echo "      |                           |"
+echo "      +---------------------------+"
+echo ""
+echo ""
+echo "Install icon theme in /usr/share/icons/"
+mkdir /usr/share/icons/revival-flat-apps-icons
+cp * /usr/share/icons/revival-flat-apps-icons -r
